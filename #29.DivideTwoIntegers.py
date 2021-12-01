de626ed31ea109012720d144fd45feddbf853b8c
AnirudhKGarg <91921469+AnirudhKGarg@users.noreply.github.com>
2021-12-01 13:40:15 -0600

    Create #29.DivideTwoIntegers.py

---
+++ #29.DivideTwoIntegers.py
@@ -0,0 +1,19 @@
+#29 divide two integers
+class Solution(object):
+    def divide(self, dividend, divisor):
+        d = abs(dividend)
+        dv = abs(divisor)
+        output = 0
+        
+        while d >= dv:
+            temp = dv
+            mul = 1
+            while d >= temp:
+                d -= temp
+                output += mul
+                mul += mul
+                temp += temp
+               
+        if (dividend < 0 and divisor >= 0) or (divisor < 0 and dividend >= 0):
+            output = -output
+        return min(2147483647, max(-2147483648, output))
